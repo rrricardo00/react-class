b6bac131d04992c0d75588badf3533f878c5cb06
rrricardo00 <rrricardo00@yahoo.com.br>
2021-10-09 01:18:35 -0300

    react

--- class1/src/App.js
+++ class1/src/App.js
@@ -1,16 +1,43 @@
+const luana = {
+  cliente: 'Luana',
+  idade: 27,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+  ],
+  ativa: true,
+};
+
+const mario = {
+  cliente: 'Mario',
+  idade: 31,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+    { nome: 'Guitarra', preco: 'R$ 3500' },
+  ],
+  ativa: false,
+};
 
 function App() {
-
-const estilo = {
-  color: "red",
-  fontSize: "2rem",
-}
+  
+  const dados = luana;
+  const total = dados.compras.map(item => +item.preco.replace('R$ ', '')).reduce(function (valor, valorContador) {
+    return valor + valorContador
+  })
 
   return (
-   <div style={estilo}>
-     Início aqui começa
-   </div>
-  );
+    <>
+      <p>Nome: {dados.cliente}</p>
+      <p> Idade: {dados.idade}</p>
+      <p>Total Gasto: <span style={{color: dados.ativa ? 'green' : 'red'}}>{total}</span></p>
+      <p>Ativa: {dados.ativa ? 'Ativa' : 'Não ativo'}</p>
+      {total > 10000 && <p>Você está gastando muito</p>}
+    </>
+  )
+
 }
 
 export default App;
