dc44fe8623c6bd13e7a936de07993fa3b2a4ffd2
rrricardo00 <rrricardo00@yahoo.com.br>
2021-10-09 01:22:27 -0300

    react

---
+++ class1/src/Arrais.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Arrais = () => {
+    return (
+        <div>
+            aqui o arrays
+        </div>
+    )
+}
+
+export default Arrais
--- class1/src/index.js
+++ class1/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Arrais from './Arrais';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Arrais/>
   </React.StrictMode>,
   document.getElementById('root')
 );
