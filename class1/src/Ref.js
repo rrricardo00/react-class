fc17416df6202cf829d6e28b02fb2b18c40b997a
rrricardo00 <rrricardo00@yahoo.com.br>
2021-10-22 01:44:55 -0300

    react

--- class1/src/Ref.js
+++ class1/src/Ref.js
@@ -2,13 +2,57 @@ import React from 'react'
 
 const Ref = () => {
 
-    const video = React.useRef();
-    
+    //     const [comentario, setComentario] = React.useState([]);
+    //     const [input, setInput] = React.useState('');
+    //     const elementoInput = React.useRef();
+
+    //     function handleClick() {
+    //         setComentario([...comentario, input])
+    //         setInput('')
+    //         elementoInput.current.focus()
+    //     }
+
+    //     React.useEffect(() => {
+    //         elementoInput.current.focus()
+    //     }, [])
+
+    //     return (
+    //         <div>
+
+    //             <input ref={elementoInput} type="text" value={input} onChange={({ target }) => setInput(target.value)} />
+
+    //             <button onClick={handleClick}>Enviar</button>
+
+    //             <ul>
+    //                 {comentario.map((comentario, index) => <li key={index}>{comentario}</li>)}
+    //             </ul>
+    //         </div>
+    //     )
+
+
+    const [carrinho, setCarrinho] = React.useState(0)
+    const [notificar, setNotificacao] = React.useState(null)
+    const referencia = React.useRef()
+
+    function handleClick() {
+        setCarrinho(carrinho + 1)
+        setNotificacao('Item adicionado ao carrinho')
+
+        clearTimeout(referencia.current)
+        referencia.current = setTimeout(() => {
+             setNotificacao(null)   
+        }, 1000);
+    }
+
+
+
     return (
         <div>
-           <div ref={video}>Teste</div>
+            <p>{notificar}</p>
+            <button onClick={handleClick}>Adicionar ao Carrinho </button>
+            <button>{carrinho}</button>
         </div>
     )
-}
 
+}
 export default Ref
