1341f12eeb0766dd1f1e7551be515a5033d10a61
Ricardo Rosa <rrricardo00@yahoo.com.br>
2021-11-04 18:13:18 -0300

    react

--- class1/src/Formulario/CheckBox.js
+++ class1/src/Formulario/CheckBox.js
@@ -2,14 +2,33 @@ import React from 'react'
 
 const CheckBox = () => {
 
-    const [valor, setValor] = React.useState(false)
+    // const [valor, setValor] = React.useState(false)
 
-    const handleChange = ({target}) => setValor(target.checked)
+    // const handleChange = ({target}) => setValor(target.checked)
+
+    // return (
+    //     <div>
+    //         <label><input onChange={handleChange} checked={valor} type="checkbox"/>Aceito os termos</label>
+    //         {valor && <p>Termos aceitos</p>}
+    //     </div>
+    // )
+
+    const [cores, setCores] = React.useState(['vermelho'])
+
+    const handleChange = ({ target }) => {
+        if (target.checked)
+            setCores([...cores, target.value])
+        else
+            setCores(cores.filter(item => item !== target.value))
+    }
+
+    const checkInclude = (cor) => cores.includes(cor)
 
     return (
         <div>
-            <label><input onChange={handleChange} checked={valor} type="checkbox"/>Aceito os termos</label>
-            {valor && <p>Termos aceitos</p>}
+            <label><input onChange={handleChange} checked={checkInclude('azul')} value="azul" type="checkbox" />Azul</label><br />
+            <label><input onChange={handleChange} checked={checkInclude('vermelho')} value="vermelho" type="checkbox" />Vermelho</label><br />
+            {cores}
         </div>
     )
 }
